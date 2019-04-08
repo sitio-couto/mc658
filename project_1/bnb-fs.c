d233f660fbf52ff2842698b74501efaa02121f2f
VFerrari <vferrari@mpc.com.br>
2019-04-07 21:26:28 -0300

    Weird results. Algorithm now following regular bnb

--- project_1/bnb-fs.c
+++ project_1/bnb-fs.c
@@ -41,7 +41,7 @@ int main(int argc, char* argv[]){
 }
 
 void bnb(int *n_nodes){
-  node *min_node;
+  node *min_node, *new_node;
   int i;
 
   // Debug limit changer
@@ -53,46 +53,50 @@ void bnb(int *n_nodes){
       
     min_node = heap.top();
     heap.pop();
-    
-    if (best_dual == best_primal) break; // If optimal result achieved, end loop
+        
     (*n_nodes)++; // Node is maturing
 
-    // Check if prune is possible (limitant)
-    // If my best solution in this node is worst than a known solution, kill it
-    if (min_node->dual > best_primal){
-      free(min_node);
-      continue;
-    }
-
-    // Try to update optimal primal bound
-    if (min_node->primal < best_primal) {
-      best_primal = min_node->primal;
-      t_best_primal = curr_time();
-      copy_best_node(min_node); // TODO: only for debugging
-    }
-
-    // Try to update optimal dual bound only if the node is not pruned by limitant
-    if (min_node->dual > best_dual){
-      best_dual = min_node->dual;
-      t_best_dual = curr_time();
-    }
-
-    // Check if prune is possible (optimality)
-    // If the primal bound for this node is already optimal, kill it
-    if (min_node->primal == min_node->dual) {
-      free(min_node);
-      continue;
-    }
-
     // Expand min_nodes child nodes
-    for (i = 0; i < n_tasks; ++i)
-      if(min_node->result[i] == 0) // if task i not part of solution yet, expand
-        heap.push(add_node(min_node, i));
+    for (i = 0; i < n_tasks; ++i){
+        if(min_node->result[i] == 0) {// if task i not part of solution yet, expand
+            new_node = add_node(min_node, i); 
+            
+            // Check if prune is possible (limitant)
+            // If my best solution in this node is worst than a known solution, kill it
+            if (new_node->dual > best_primal)
+                free(new_node);
+            else{
+                
+                // Try to update optimal primal bound
+                if (new_node->primal < best_primal) {
+                    best_primal = new_node->primal;
+                    t_best_primal = curr_time();
+                    copy_best_node(new_node); // TODO: only for debugging
+                }
 
+                // Try to update optimal dual bound
+                if (new_node->dual > best_dual){
+                    best_dual = new_node->dual;
+                    t_best_dual = curr_time();
+                } 
+            
+                // Check if prune is possible (optimality)
+                // If the primal bound for this node is already optimal, kill it
+                if(new_node->primal == new_node->dual)
+                    free(new_node);
+                else
+                    heap.push(new_node);
+            }
+        }
+    }
+    
     free(min_node);
+
+    // Complete solution
+    if (best_dual == best_primal)
+        break;
   }
 
-    if (min_node) free(min_node); // check if null first
     return;
 }
 
