2ef4db90832ca2999d2a4166c14f58182ebeac95
vinicius espindola <vinicius.c.e@hotmail.com>
2019-05-27 19:42:57 -0300

    parameter changer done

--- project_3/search_type.py
+++ project_3/search_type.py
@@ -9,26 +9,31 @@ content = code.read()
 # int_search call
 start = content.find("(", content.find("int_search"))+1
 end   = content.find(")",start)
-content = list(content)
 
 if start == -1 or end == -1:
     print("ERROR: Could not find 'int_search' call in pert.mzn")
 elif len(argv) < 3:
     print("ERROR: Missing arguments for search_type.py")
 else:
-    # print(content[start:end])
-    params = [start-1]
-    params += [start+i for i,x in enumerate(content[start:end]) if x == ","]
-    params += [end]
     
-    # Change var choice
-    if argv[1] != '0':
-        content[params[1]+1:params[2]] = argv[1]
-    # Change constraint choice
-    if argv[2] != '0':
-        content[params[2]+1:params[3]] = argv[2]
+    for i in range(1,3):
+        s = [start-1]
+        s += [start+j for j,x in enumerate(content[start:end]) if x == ","]
+        s += [end]
+
+        # Change var choice
+        if argv[i] != '0':
+            content = content[:s[i]+1]+" "+argv[i]+content[s[i+1]:]
+
+    # print(content[start:end])
+    # print(list(map(lambda x : content[x],s)))
+    # print(content[s[1]+1:s[2]])
+    # print(content[s[2]+1:s[3]])
+
+    
+    
     
 
-test.write("".join(content))
+test.write(content)
 code.close()
 test.close()
