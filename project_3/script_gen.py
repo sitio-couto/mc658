e980226ce05c717e4fe3ac37f9fb4385c2dc5300
vinicius espindola <vinicius.c.e@hotmail.com>
2019-05-28 08:06:27 -0300

    Small changes to script gen

--- project_3/script_gen.py
+++ project_3/script_gen.py
@@ -1,4 +1,4 @@
-
+from sys import argv
 
 # Lists with all possible values for var and contraint choice
 varchoice = ["smallest","first_fail","dom_w_deg","most_constrained",
@@ -21,27 +21,58 @@ large = ["Ins_10o_100j_A.dzn", "Ins_10o_102j_A.dzn", "Ins_10o_106j_A.dzn", "Ins_
          "Ins_10o_84j_B.dzn", "Ins_10o_84j_B.dzn", "Ins_10o_87j_A.dzn", "Ins_10o_88j_A.dzn",
          "Ins_12o_108j_A.dzn", "Ins_12o_109j_A.dzn"]
 
-splitter = "\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%"
-time_limit = 180*1000
-file_name = "testing.mzn"
-out_file = "results.txt"
-script_name = "run_script.sh"
-command = "minzinc "+file_name+" -s -t "+str(time_limit)
+# Get output file to be create with results
+if len(argv) >= 4:
+    time_limit = int(argv[1])*1000
+    out_file = argv[2]
+else:
+    print("MISSING ARGUMENTS: <time(s)> <output_file> <type_gen>")
+    exit(1)
 
-params = []
+splitter = "\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%"
+file_name = "testing.mzn"
+script_name = "run_script.sh"
+command = "minizinc "+file_name+" -s -t "+str(time_limit)
+
+vcs = []
+ccs = []
 for vc in varchoice[:4]:
-    for cc in constchoice[:4]:
-        params.append((vc,cc))
+    vcs.append(vc)
+for cc in constchoice[:4]:
+    ccs.append(cc)
 
 output = []
-for i in small:
-    for (vc,cc) in params:
-        output.append("search_type.py "+vc+" "+cc)
-        output.append('echo "'+splitter+'"'+" >> "+out_file)
-        output.append('echo "' +"PARAMS: "+vc+" "+cc+'" >> '+out_file)
-        output.append('echo "'+'INSTANCE: '+i+'" >> '+out_file)
-        output.append(command+" Instance "+i+" >> "+out_file)
+# Generate only chaging varchoices
+if (argv[3]=="1"):
+    for i in small:
+        for vc in vcs:
+            output.append("\npython search_type.py "+vc+" "+cc)
+            output.append('\necho "'+splitter+'"'+" >> "+out_file)
+            output.append('\necho "' +"PARAMS: "+vc+" "+"0"+'" >> '+out_file)
+            output.append('\necho "'+'INSTANCE: '+i+'" >> '+out_file)
+            output.append("\n"+command+" Instances/"+i+" >> "+out_file)
+# Generate only changing constraintchoices
+elif (argv[3]=="2"):
+    for i in small:
+        for cc in ccs:
+            output.append("\npython search_type.py "+vc+" "+cc)
+            output.append('\necho "'+splitter+'"'+" >> "+out_file)
+            output.append('\necho "' +"PARAMS: "+"0"+" "+cc+'" >> '+out_file)
+            output.append('\necho "'+'INSTANCE: '+i+'" >> '+out_file)
+            output.append("\n"+command+" Instances/"+i+" >> "+out_file)
+# Generate combining both var and contraint choices
+elif (argv[3]=="3"):
+    for i in small:
+        for vc in vcs:
+            for cc in ccs:
+                output.append("\npython search_type.py "+vc+" "+cc)
+                output.append('\necho "'+splitter+'"'+" >> "+out_file)
+                output.append('\necho "' +"PARAMS: "+vc+" "+cc+'" >> '+out_file)
+                output.append('\necho "'+'INSTANCE: '+i+'" >> '+out_file)
+                output.append("\n"+command+" Instances/"+i+" >> "+out_file)
+    
 
 script = open(script_name,"w")
-for i in output:
-    print(i)
+map(script.write,output)
+script.write("\n")
+script.close()
